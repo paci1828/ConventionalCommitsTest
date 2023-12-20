d5dccec05b38b4583cae4284e257a1e2cf022f63
Simo Paci <simone.pacileo@piksel.com>
2023-12-20 17:36:23 +0100

    feat: add new api

--- demo/update-changelog.sh
+++ demo/update-changelog.sh
@@ -11,12 +11,18 @@ if git log -n 1 --pretty=format:%s | grep -E '^\s*\*\*\[(feat|fix|refactor)\]'; 
   # Rimuovi le sezioni indesiderate dal changelog
   awk '/^## / {if (++n == 1) print $0; next} /^# Changelog/ {n=0} /^All notable changes/ {n=0} !n {print $0}' CHANGELOG.md > tmp_changelog.md
 
-  # Sovrascrivi il changelog originale con quello aggiornato
-  mv tmp_changelog.md CHANGELOG.md
+  # Sovrascrivi il changelog originale con quello aggiornato solo se ci sono aggiornamenti significativi
+  # shellcheck disable=SC2143
+  if [[ "$(grep -E '^(\s*\*\*\[feat\]|(\s*\*\*\[fix\]|\s*\*\*\[refactor\]))' tmp_changelog.md)" ]]; then
+    mv tmp_changelog.md CHANGELOG.md
 
-  # Fai il commit del changelog aggiornato
-  git add CHANGELOG.md
-  git commit -m "chore(release): aggiorna changelog per la versione $new_version"
+    # Fai il commit del changelog aggiornato
+    git add CHANGELOG.md
+    git commit -m "chore(release): aggiorna changelog per la versione $new_version"
+  else
+    echo "Nessun aggiornamento significativo rilevato. La versione rimane invariata."
+    rm tmp_changelog.md
+  fi
 else
-  echo "Nessun aggiornamento significativo rilevato. La versione rimane invariata."
+  echo "Nessun commit rilevante per l'auto-incremento del changelog. La versione rimane invariata."
 fi
