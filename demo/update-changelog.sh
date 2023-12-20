8ff18b3bcb1c706527ad8cf0821f6fe90ecc146c
Simo Paci <simone.pacileo@piksel.com>
2023-12-20 17:40:00 +0100

    fix: remove api

--- demo/update-changelog.sh
+++ demo/update-changelog.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Verifica se ci sono nuove features, bug fix o refactoring nella storia dei commit
-if git log -n 1 --pretty=format:%s | grep -E '^\s*\*\*\[(feat|fix|refactor)\]'; then
+# Conta il numero di commit con feat, fix o refactor nella storia
+count=$(git log --pretty=format:%s | grep -E '^\s*\*\*\[(feat|fix|refactor)\]' | wc -l)
+
+if [ $count -gt 0 ]; then
   # Esegui standard-version per incrementare la versione e generare il changelog
   standard-version -a --commit-all --releaseCommitMessageFormat "chore(release): %s"
 
@@ -12,7 +14,6 @@ if git log -n 1 --pretty=format:%s | grep -E '^\s*\*\*\[(feat|fix|refactor)\]'; 
   awk '/^## / {if (++n == 1) print $0; next} /^# Changelog/ {n=0} /^All notable changes/ {n=0} !n {print $0}' CHANGELOG.md > tmp_changelog.md
 
   # Sovrascrivi il changelog originale con quello aggiornato solo se ci sono aggiornamenti significativi
-  # shellcheck disable=SC2143
   if [[ "$(grep -E '^(\s*\*\*\[feat\]|(\s*\*\*\[fix\]|\s*\*\*\[refactor\]))' tmp_changelog.md)" ]]; then
     mv tmp_changelog.md CHANGELOG.md
 
