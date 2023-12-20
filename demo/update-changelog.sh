738e3f4d2680fb68fef744a5ebec6c5b29dd4809
Simo Paci <simone.pacileo@piksel.com>
2023-12-20 16:29:07 +0100

    modify script

--- demo/update-changelog.sh
+++ demo/update-changelog.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+##!/bin/bash
+#
+## Esegui standard-version per incrementare la versione e generare il changelog
+#standard-version -a --commit-all --releaseCommitMessageFormat "chore(release): %s"
+#
+## Estrai la versione appena generata dal package.json
+#new_version=$(awk -F'"' '/"version":/ {print $4}' package.json)
+#
+## Ottieni le informazioni sul changelog dal file generato
+#changelog_content=$(cat CHANGELOG.md)
+#
+## Scrivi le informazioni nel changelog
+#echo -e "## $new_version ($(date +"%Y-%m-%d"))\n\n$changelog_content" > CHANGELOG.md
+#
+## Fai il commit del changelog aggiornato
+#git add CHANGELOG.md
+#git commit -m "chore(release): aggiorna changelog per la versione $new_version"
+
+
+
 
 # Esegui standard-version per incrementare la versione e generare il changelog
 standard-version -a --commit-all --releaseCommitMessageFormat "chore(release): %s"
@@ -9,9 +28,17 @@ new_version=$(awk -F'"' '/"version":/ {print $4}' package.json)
 # Ottieni le informazioni sul changelog dal file generato
 changelog_content=$(cat CHANGELOG.md)
 
-# Scrivi le informazioni nel changelog
-echo -e "## $new_version ($(date +"%Y-%m-%d"))\n\n$changelog_content" > CHANGELOG.md
+# Rimuovi l'intestazione standard
+changelog_content=$(echo "$changelog_content" | awk '!/^# Change Log$/{print}')
 
-# Fai il commit del changelog aggiornato
-git add CHANGELOG.md
-git commit -m "chore(release): aggiorna changelog per la versione $new_version"
+# Controlla se ci sono versioni con features, bug fix o refactoring
+if [[ "$changelog_content" =~ ^##\ $new_version.*\n\ \*\*\[.*\]\*\*.*$ ]]; then
+  # Scrivi le informazioni nel changelog
+  echo -e "## $new_version ($(date +"%Y-%m-%d"))\n\n$changelog_content" > CHANGELOG.md
+
+  # Fai il commit del changelog aggiornato
+  git add CHANGELOG.md
+  git commit -m "chore(release): aggiorna changelog per la versione $new_version"
+else
+  echo "Nessun aggiornamento significativo per la versione $new_version."
+fi
