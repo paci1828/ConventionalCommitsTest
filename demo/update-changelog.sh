2e8e13dd054acc8230afad83d5b21ba459c46538
Simo Paci <simone.pacileo@piksel.com>
2023-12-20 16:45:48 +0100

    modify script

--- demo/update-changelog.sh
+++ demo/update-changelog.sh
@@ -1,44 +1,22 @@
-##!/bin/bash
-#
-## Esegui standard-version per incrementare la versione e generare il changelog
-#standard-version -a --commit-all --releaseCommitMessageFormat "chore(release): %s"
-#
-## Estrai la versione appena generata dal package.json
-#new_version=$(awk -F'"' '/"version":/ {print $4}' package.json)
-#
-## Ottieni le informazioni sul changelog dal file generato
-#changelog_content=$(cat CHANGELOG.md)
-#
-## Scrivi le informazioni nel changelog
-#echo -e "## $new_version ($(date +"%Y-%m-%d"))\n\n$changelog_content" > CHANGELOG.md
-#
-## Fai il commit del changelog aggiornato
-#git add CHANGELOG.md
-#git commit -m "chore(release): aggiorna changelog per la versione $new_version"
-
-
-
-
+#!/bin/bash
 # Esegui standard-version per incrementare la versione e generare il changelog
 standard-version -a --commit-all --releaseCommitMessageFormat "chore(release): %s"
 
 # Estrai la versione appena generata dal package.json
 new_version=$(awk -F'"' '/"version":/ {print $4}' package.json)
 
-# Ottieni le informazioni sul changelog dal file generato
-changelog_content=$(cat CHANGELOG.md)
-
 # Rimuovi le sezioni indesiderate dal changelog
-changelog_content=$(echo "$changelog_content" | awk '/^[0-9]+\.[0-9]+\.[0-9]+/{flag=1} /^#/{flag=0} !flag {print}')
+awk '/^## / {if (++n == 1) print $0; next} /^# Changelog/ {n=0} /^All notable changes/ {n=0} !n {print $0}' CHANGELOG.md > tmp_changelog.md
 
 # Controlla se ci sono versioni con features, bug fix o refactoring
-if [[ "$changelog_content" =~ ^##\ $new_version.*\n\ \*\*\[.*\]\*\*.*$ ]]; then
-  # Scrivi le informazioni nel changelog
-  echo -e "## $new_version ($(date +"%Y-%m-%d"))\n\n$changelog_content" > CHANGELOG.md
+if [[ "$(cat tmp_changelog.md)" =~ ^##\ $new_version.*\n\ \*\*\[.*\]\*\*.*$ ]]; then
+  # Sovrascrivi il changelog originale con quello aggiornato
+  mv tmp_changelog.md CHANGELOG.md
 
   # Fai il commit del changelog aggiornato
   git add CHANGELOG.md
   git commit -m "chore(release): aggiorna changelog per la versione $new_version"
 else
   echo "Nessun aggiornamento significativo per la versione $new_version."
+  rm tmp_changelog.md
 fi
