616b626809383612ef2424e179395d4ead827021
Simo Paci <simone.pacileo@piksel.com>
2023-12-21 17:14:43 +0100

    chore(release): v0.0.1
        Commitizen has two command line tools:
    
          1) cz         -- used for making commits according to convention
                           note: you can run 'git cz' if installed with -g
          2) git-cz     -- alias for 'cz'
          3) commitizen -- used for installing adapters into your project
    
        Generally if you're using someone else's repo and they've already set up an
        adapter, you're going to just be running:
    
             cz
    
        However, if you create a new repo and you want to make it easier for future
        contributors to follow your commit message conventions using commitizen then
        you'll need to run a command like this one to add this adapter to your config:
    
             commitizen init cz-conventional-changelog --save
    
        You should swap out cz-conventional-changelog for the NPM package name of the
        adapter you wish you install in your project's package.json.
    
        Detailed usage:
    
          1) commitizen <sub-command>
    
             init <adapter-npm-name> [args]
    
               description: Install a commitizen adapter from npm and adds it to your
                            config.commitizen in your package.json file.
    
               args:
                 --save         Install the adapter to package.json dependencies
                 --save-dev     Install the adapter to devDependencies
                 --save-exact   Install an exact version instead of a range
                 --force        Force install the adapter, even if a previous one exists.
    
          2) cz <any regular git commit arguments>
    
              description: Runs the commitizen prompter, asking you questions so that you
                           follow the commit conventions of the repository of the current
                           directory.
    
                           note: cz may even be run as 'git cz' if installed with -g.

---
+++ demo/script-changelog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Increment version number
+VERSION=$(npm version patch --no-git-tag-version)
+
+# Get last version from changelog
+LAST_VERSION=$(grep -m1 -oP '(?<=## )\d+\.\d+\.\d+' CHANGELOG.md || echo $VERSION)
+
+# Create commit message
+COMMIT_MESSAGE=$(echo -e "chore(release): $VERSION\n\n$(git log --pretty=format:'%s' $LAST_VERSION..HEAD | commitizen)")
+
+# Commit changes
+git add .
+git commit -m "$COMMIT_MESSAGE"
+
+# Generate changelog
+npx standard-version --skip.changelog
+
+# Push changes
+git push
+git push --tags
