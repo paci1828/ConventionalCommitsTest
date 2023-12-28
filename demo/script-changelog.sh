aca3c42a4d5b40ff5a37b2521991d018f94a6996
Simo Paci <simone.pacileo@piksel.com>
2023-12-28 09:16:29 +0100

    feat : add new api

--- demo/script-changelog.sh
+++ demo/script-changelog.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-set -e
+# Importa la libreria commitizen
+source "$(dirname "$0")/commitizen"
 
-# Increment version number
-VERSION=$(npm version patch --no-git-tag-version)
+# Determina la versione corrente del changelog
+# shellcheck disable=SC2002
+current_version=$(cat CHANGELOG.md | grep "^## Version" | awk '{print $2}')
 
-# Get last version from changelog
-LAST_VERSION=$(grep -m1 -oP '(?<=## )\d+\.\d+\.\d+' CHANGELOG.md || echo "$VERSION")
+# Controlla se sono stati effettuati dei commit
+if [ "$(git log --oneline | wc -l)" -gt 0 ]; then
 
-# Create commit message
-COMMIT_MESSAGE=$(echo -e "chore(release): $VERSION\n\n$(git log --pretty=format:'%s' "$LAST_VERSION"..HEAD | commitizen)")
+    # Genera il nuovo changelog
+    new_changelog=$(cz generate)
 
-# Commit changes
-git add .
-git commit -m "$COMMIT_MESSAGE"
+    # Aggiorna la versione del changelog
+    new_version=$(echo "$new_changelog" | grep "^## Version" | awk '{print $2}')
 
-# Generate changelog
-npx standard-version --skip.changelog
+    # Controlla se la versione è cambiata
+    if [ "$new_version" != "$current_version" ]; then
 
-# Push changes
-git push
-git push --tags
+        # Scrivi il nuovo changelog
+        echo "$new_changelog" > CHANGELOG.md
+
+        # Aggiorna la versione corrente
+        current_version="$new_version"
+
+    fi
+
+fi
+
+# Stampa la versione corrente del changelog
+echo "Versione corrente del changelog: $current_version"
