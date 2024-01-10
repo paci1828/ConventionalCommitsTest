ac14c6164fd1955c4cf6d934e5762be5ddbbebbc
Simo Paci <simone.pacileo@piksel.com>
2024-01-10 10:08:01 +0100

    refactor(delete-a-method): delete method 1 and method 2 in controller

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,13 +1,7 @@
 package com.example.demo;
 
 public class Controller {
-    public void Method1(){
-    }
 
-
-    public void Method2(){
-
-    }
 }
 
 
--- demo/src/main/java/com/example/demo/Service.java
+++ demo/src/main/java/com/example/demo/Service.java
@@ -1,6 +1,4 @@
 package com.example.demo;
 
 public class Service {
-
-
 }
